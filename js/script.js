bcb6b4cfc82e891966323151ac0997ae3bb9bdec
YASSINE EL MAZOUGHI <116319288+ELMAZOUGH@users.noreply.github.com>
2024-12-09 23:15:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Fullstack Developer', 'Devops Engineer', 'Cloud Engineer'],
+    strings: ['QA Engineer', ' Test Automation Engineer ', 'Continuous Integration (CI) Test Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
